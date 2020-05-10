a223785dc1a2859136f5cd9efd56b142a5bc6b48
Alex Hou <houjun19830610@126.com>
2020-05-10 14:10:07 +0800

    Update instance.tf
    Signed-off-by: Alex Hou <houjun19830610@126.com>

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,14 +5,14 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "ap-east-1"
+  default = "us-east-2"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    ap-east-1 = ""
     us-east-2 = "ami-0f7919c33c90f5b58"
+#   us-east-1 = "ami-xxxxxxxxxxxxxxxxx"
   }
 }
 
