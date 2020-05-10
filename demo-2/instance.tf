9866f82b709ea682069ee95971f06b9c2a79885a
Alex Hou <houjun19830610@126.com>
2020-05-10 14:17:09 +0800

    Update instance.tf
    Signed-off-by: Alex Hou <houjun19830610@126.com>

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -15,14 +15,17 @@ resource "aws_instance" "example" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/script.sh",
-      "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters. \r$ means ends in \r which is used in Windows.
+      "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters. 
+# \r$ means ends in \r which is used in Windows.
       "sudo /tmp/script.sh",
     ]
   }
   connection {
-    host        = coalesce(self.public_ip, self.private_ip) # coalesce function gives the first value which is not null, each parameter should be the same type
-#    host        = "self.private_ip"
-    type        = "ssh" # another is winrm
+    host        = coalesce(self.public_ip, self.private_ip) 
+# coalesce function gives the first value which is not null, each parameter should be the same type
+# Or you may try below as private_ip doesn't work sometimes:
+#   host        = "self.public_ip"
+    type        = "ssh" # Another is winrm, for Windows
     user        = var.INSTANCE_USERNAME
     private_key = file(var.PATH_TO_PRIVATE_KEY)
   }
