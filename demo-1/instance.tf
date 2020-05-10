b23ce329597ae7fa3f283b37c5f27e1bb021cf19
Alex Hou <houjun19830610@126.com>
2020-05-10 10:08:10 +0800

    Update instance.tf
    Signed-off-by: Alex Hou <houjun19830610@126.com>

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = var.AMIS[var.AWS_REGION]
-#  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+#  ami           = "${lookup(var.AMIS, var.AWS_REGION)}", this is deprecated
   instance_type = "t2.micro"
 }
 
